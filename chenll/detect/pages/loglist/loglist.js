095d2771ee0e141eac33a3d590b9cd0da8a032e6
chenlingling1985 <435906852@qq.com>
2019-11-01 19:35:03 +0800

    +

--- chenll/detect/pages/loglist/loglist.js
+++ chenll/detect/pages/loglist/loglist.js
@@ -6,14 +6,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-    list:[],
-id:""
+    list: [],
+    id: ""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     var that = this;
     let eqpid = options.id;
     wx.request({
@@ -28,109 +28,109 @@ id:""
         "Content-Type": "application/x-www-form-urlencoded"
       },
 
-      success: function (res) {
+      success: function(res) {
         //将获取到的json数据，存在名字叫list的这个数组中
         console.log(res.data.mtclog),
           that.setData({
             list: res.data.mtclog,
-            id:eqpid
+            id: eqpid
             //res代表success函数的事件对，data是固定的，list是数组
           })
 
       },
-      fail: function (err) {
+      fail: function(err) {
         console.log("失败")
       }
 
     })
   },
-  detail:function(e){
+  detail: function(e) {
     console.log(e);
     wx.navigateTo({
       url: '../logdetail/logdetail?id=' + e.currentTarget.id,
 
-      success: function () {
+      success: function() {
         console.log('接口调用成功')
 
       },
-      fail: function () {
+      fail: function() {
         console.log('接口调用成功')
       },
-      complete: function () {
+      complete: function() {
         console.log('调用结束，调用成功失败都会执行')
       }
     })
-  
+
 
   },
-  addlog:function(){
+  addlog: function() {
     var id = this.data.id;
     console.log('接口调用成功传参数' + this.data.id)
     wx.navigateTo({
-      
+
 
       url: '../logadd/logadd?id=' + id,
-      
 
-      success: function () {
-        console.log('接口调用成功传参数'+id)
+
+      success: function() {
+        console.log('接口调用成功传参数' + id)
 
       },
-      fail: function () {
+      fail: function() {
         console.log('接口调用成功')
       },
-      complete: function () {
+      complete: function() {
         console.log('调用结束，调用成功失败都会执行')
       }
     })
   },
-  
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
