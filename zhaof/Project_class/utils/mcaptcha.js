5cf7b5e6b99f4f13749e19676dacc07f9945f3c3
zhaof33 <5501338@qq.com>
2019-11-04 17:04:46 +0800

    over

--- zhaof/Project_class/utils/mcaptcha.js
+++ zhaof/Project_class/utils/mcaptcha.js
@@ -12,11 +12,13 @@ module.exports = class Mcaptcha {
   }
   refresh() {
     var code = '';
-    var txtArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+   var txtArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     for (var i = 0; i < 4; i++) {
       code += txtArr[this.randomNum(0, txtArr.length)];
     }
+    code = this.options.code
     this.options.createCodeImg = code;
+
     let arr = (code + '').split('');
     if (arr.length === 0) {
       arr = ['e', 'r', 'r', 'o', 'r'];
