5f4a68c7f876a28af9717ba163c68b727ba3d6e1
chenlingling1985 <435906852@qq.com>
2019-11-11 08:49:44 +0800

    20191111版本

--- smartPatrolok/pages/dev/eqpmod.js
+++ smartPatrolok/pages/dev/eqpmod.js
@@ -3,129 +3,110 @@ const app = getApp();
 var util = require('../../utils/util.js');
 Page({
 
-  /**
-   * 页面的初始数据
+  /*
+    id:设备编码
+    list:页面显示数据
    */
+
   data: {
     id: "",
     list: {}
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  //页面加载时以设备编号查询设备信息
   onLoad: function(options) {
     var that = this;
     let eqpid = options.id;
-    console.log("123")
     wx.request({
-
       url: 'http://112.93.119.181:8090/zhyw/api/equipment/',
       method: 'post',
       data: util.json2Form({
         id: eqpid
       }),
-
       header: {
         "Content-Type": "application/x-www-form-urlencoded"
       },
-
-
       success: function(res) {
-        //将获取到的json数据，存在名字叫list的这个数组中
-        console.log(res.data),
-          that.setData({
-            list: res.data,
-            id: eqpid
-            //res代表success函数的事件对，data是固定的，list是数组
-          })
-
+        that.setData({
+          list: res.data,
+          id: eqpid
+        })
       },
       fail: function(err) {
-        console.log("失败")
+        // console.log("失败")
       }
-
     })
-
   },
 
+  // 提交表单修改数据库中当前信息
   formSubmit: function(e) {
     var that = this;
-    console.log("修订：" + that.data.id);
-    console.log("提交结果" + e.detail.value.address);
-    wx.request({
-
-
-      url: 'http://112.93.119.181:8090/zhyw/api/eqpedit/',
-      method: 'post',
-      data: util.json2Form({
-        eqp_id: that.data.id,
-        mtc_type: "数据修订",
-        maintainer_name: "陈玲玲",
-        maintainer_tel: "18603377371",
-        description: "数据修订",
-        address: e.detail.value.address,
-        longtitude: e.detail.value.longtitude,
-        latitude: e.detail.value.latitude,
-        principal_name: e.detail.value.principal_name,
-        principal_tel: e.detail.value.principal_tel,
-        mfr_tel: e.detail.value.mfr_tel,
-        comment: e.detail.value.comment,
-      }),
-
-
-      header: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      },
-
-
-      success: function(res) {
-        //将获取到的json数据，存在名字叫list的这个数组中
-        console.log(res.data),
-          that.setData({
-
-            //res代表success函数的事件对，data是固定的，list是数组
+    let address = e.detail.value.address,
+      longtitude = e.detail.value.longtitude,
+      latitude = e.detail.value.latitude,
+      principal_name = e.detail.value.principal_name,
+      principal_tel = e.detail.value.principal_tel,
+      mfr_tel = e.detail.value.mfr_tel,
+      comment = e.detail.value.comment;
+    if ((address == "") || (longtitude == "") || (latitude == "") || (principal_name == "") || (principal_tel == "") || (mfr_tel == "") || (comment == "")) {
+      wx.showToast({
+        title: '数据都不为空',
+        icon: 'success',
+        duration: 2000
+      })
+    } else if (parseInt(address.length) > 100) {
+      wx.showToast({
+        title: '地址过长',
+        icon: 'success',
+        duration: 2000
+      })
+    } else if (parseInt(comment.length) > 2000) {
+      wx.showToast({
+        title: '备注过长',
+        icon: 'success',
+        duration: 2000
+      })
+    } else {
+      wx.request({
+        url: 'http://112.93.119.181:8090/zhyw/api/eqpedit/',
+        method: 'post',
+        data: util.json2Form({
+          eqp_id: that.data.id,
+          mtc_type: "数据修订",
+          maintainer_name: app.globalData.name,
+          maintainer_tel: app.globalData.mobile,
+          description: "数据修订",
+          address: address,
+          longtitude: longtitude,
+          latitude: latitude,
+          principal_name: principal_name,
+          principal_tel: principal_tel,
+          mfr_tel: mfr_tel,
+          comment: comment
+        }),
+        header: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        success: function(res) {
+          wx.showToast({
+            title: '成功',
+            icon: 'success',
+            duration: 2000
           })
-        wx.showToast({
-          title: '成功',
-          icon: 'success',
-          duration: 2000
-        })
-        wx.navigateBack({
-          delta: 1 // 返回上一页
-        });
+          wx.navigateBack({
+            delta: 1 // 返回上一页
+          });
 
-      },
-      fail: function(err) {
-        wx.showToast({
-          title: '失败',
-          icon: 'success',
-          duration: 2000
-        })
-      }
-
-    })
-  },
-  formReset: function(e) {
-    // var that = this;
-    // e.detail.value.address = "",
-    //   e.detail.value.longtitude = "",
-    //   e.detail.value.latitude = "",
-    //   e.detail.value.principal_name = "",
-    //   e.detail.value.principal_tel = "",
-    //   e.detail.value.mfr_tel = "",
-    //   e.detail.value.comment = ""
-  },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
+        },
+        fail: function(err) {
+          wx.showToast({
+            title: '失败',
+            icon: 'success',
+            duration: 2000
+          })
+        }
+      })
+    }
 
   }
-
-
 })
